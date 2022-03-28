15b8f29bb954fa77917e8f052d17dd216ec1067f
ryan rivera <rrmatthew03@yahoo.com>
2022-03-27 18:27:53 -0700

    commit tree bbaf5cda674c13afbab57db168bf0314bb344031

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// Code your solutions in this file.exit
+
+function writeCards(name){
+   console.log('Thank you [name], for the wonderful surprise gifts!')
+}
+writeCards('Guadalupe')
+writeCards('Ollie')
+writeCards('Akai')
+
+function countDown(number){
+  console.log(4)
+  console.log(3)
+  console.log(2)
+  console.log(1)
+  console,log(0)
+}
+
+countDown(number)
